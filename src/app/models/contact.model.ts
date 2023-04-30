b4bf2a1a329f7cd1eb7633449babe796e2e16821
Jhon Rey <jhonreymendiola@gmail.com>
2023-04-30 17:16:20 +0800

    working mat table with sort

---
+++ src/app/models/contact.model.ts
@@ -0,0 +1,13 @@
+export interface Contact {
+    fullName : string,	
+    mobileNumber:string,	
+    email:string,	
+    contactType:string,
+    dateUpdated:string,	
+  }
+  
+//   export interface Project{
+//     name:string,
+//     id:number
+//   }
+  
--- src/app/modules/contact/contact.module.ts
+++ src/app/modules/contact/contact.module.ts
@@ -3,14 +3,24 @@ import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact.component';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactMainComponent } from './contact-main/contact-main.component';
-
+import { MatTableModule } from '@angular/material/table' 
+import {MatSortModule} from '@angular/material/sort';
 
 
 @NgModule({
   declarations: [ContactComponent, ContactMainComponent],
   imports: [
     CommonModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    MatTableModule,
+    MatSortModule
+  ],
+  exports:[
+    MatTableModule,
+    // MatInputModule,
+    // MatSelectModule,
+    // MatCardModule,
+    MatSortModule
   ]
 })
 export class ContactModule { }
